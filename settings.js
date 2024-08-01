9ea31d5a0091e6310067dd261cdef13fd1857ba2
jamilaanas <maryamstatut@gmail.com>
2024-08-01 15:15:08 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> HSVXDX V5';
 global.owner = process.env.OWNER_NUMBER  || '212639818098' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zIt1DA5Q#KeMoeYNxSQTu2DH-g_x_U24aI2z09vtGK_TwSXOa3js' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "212639818098,212639818098" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
